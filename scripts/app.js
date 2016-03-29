0b8e3c909b9a4fb74e395abfe35ec75a7f759614
gai <tagai1106@gmail.com>
2016-03-30 01:21:33 +0900

    ページ移動してもアイコンの状態が変わらない不具合修正

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,21 @@
 //タブ切り替えごとの処理
-chrome.tabs.onActivated.addListener(function() {
+chrome.tabs.onActivated.addListener(function(){
+  changeIcon();
+});
+chrome.tabs.onUpdated.addListener(function(){
+  changeIcon();
+});
+
+
+//クリック時処理
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.executeScript(null, {
+      "file": "scripts/clickAction.js"
+  });
+});
+
+//アイコンのアクティブ状態の制御
+function changeIcon(){
   chrome.tabs.getSelected(null, function(tab) {
     if (tab.url.match(/\/\/www\.youtube\.com\/watch/g)
       || tab.url.match(/\/\/listenonrepeat\.com\/watch/g)) {
@@ -12,11 +28,4 @@ chrome.tabs.onActivated.addListener(function() {
       });
     }
   });
-});
-
-//クリック時処理
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.executeScript(null, {
-      "file": "scripts/clickAction.js"
-  });
-});
+};
